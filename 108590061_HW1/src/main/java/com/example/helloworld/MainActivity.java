d14adb8f7c84510abb56d8690c69b4582a81c91b
Huang Sheng Yao <t108590061@ntut.edu.tw>
2021-02-27 15:13:30 +0800

    HW1

---
+++ 108590061_HW1/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        //Log.e("MainActivity", "Hello World");
+    }
+}
